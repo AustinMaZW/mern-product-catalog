737f5afdb12bbcf11e1bf3c5af091eaf1d5c4723
Austin Ma <austin.ma@oracle.com>
2025-08-29 19:29:56 -0700

    changes due to express 5 with stricter regex rules

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,19 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.use(express.json()); // middleware that allows us to accept json DATA in req.body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  // fallback route (regex catch-all)
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
